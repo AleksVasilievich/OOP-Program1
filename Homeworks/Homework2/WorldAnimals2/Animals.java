8ffefbe2999528d9a968e301e3085b884a4775c6
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2023-01-22 18:59:00 +0300

    Homework2

--- Homeworks/Homework2/WorldAnimals2/Animals.java
+++ Homeworks/Homework2/WorldAnimals2/Animals.java
@@ -1,5 +1,76 @@
 package Homeworks.Homework2.WorldAnimals2;
 
-public class Animals {
+public interface Animals {
+
+    void info();
+
+}
+
+class People implements Animals {
+    public String instruction;
+
+    People(String instr) {
+        this.instruction = instr;
+
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setSpeak(String instruction) {
+        this.instruction = instruction;
+    }
+
+    @Override
+    public void info() {
+        System.out.println("People: " + instruction);
+    }
+
+}
+
+class Cat implements Animals {
+    public String instruction;
+
+    Cat(String instr) {
+        this.instruction = instr;
+
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setSpeak(String instruction) {
+        this.instruction = instruction;
+    }
+
+    @Override
+    public void info() {
+        System.out.println("Cat: " + instruction);
+    }
+
+}
+
+class Dog implements Animals {
+    public String instruction;
+
+    Dog(String instr) {
+        this.instruction = instr;
+
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public void setSpeak(String instruction) {
+        this.instruction = instruction;
+    }
+
+    @Override
+    public void info() {
+        System.out.println("Dog: " + instruction);
+    }
 
 }
