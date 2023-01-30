31b3f3b95d7ac1598ea16e87f9217d18014aacc8
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2023-01-31 02:36:32 +0300

    Add3 Homework4

--- Homeworks/Homework4/Tree4/People.java
+++ Homeworks/Homework4/Tree4/People.java
@@ -2,8 +2,6 @@ package Homeworks.Homework4.Tree4;
 
 public interface People<T> {
 
-    boolean test(T a); // Добавил стандартный интерфейс
-
-    // void info(); // Этот метод вызываю отдельно в каждом следующем подклассе
+    boolean test(T a); // Добавил обобщение
 
 }
